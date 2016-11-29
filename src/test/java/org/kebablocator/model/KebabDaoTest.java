313ed07c8b547ebbd3cf38b86b556282b9c7751a
ebongue <chrisebongue@hotmail.fr>
2016-11-29 23:04:23 +0100

    update kebabDaoTest

--- src/test/java/org/kebablocator/model/KebabDaoTest.java
+++ src/test/java/org/kebablocator/model/KebabDaoTest.java
@@ -9,9 +9,12 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -27,22 +30,26 @@ public class KebabDaoTest {
     private KebabDao kebabDao;
 
     @Test()
-    public void should_return_exception_if_no_kebab(){
+    public void should_return_no_kebab(){
         int id = 1;
-        assertTrue(kebabDao.exists(id));
-        kebabDao.findById(id);
+
+        assertTrue(kebabDao.findById(id) instanceof Kebab);
     }
 
     @Test
     public void should_valid_if_this_kebab_in_database(){
          int id = 1;
         assertTrue(kebabDao.exists(id));
-        kebabDao.findById(id);
     }
 
     @Test
     public void should_return_all_kebab(){
-        List<Kebab> kebabList = new LinkedList<>();
-        kebabDao.findAll();
+        List<Kebab> kebabList = kebabDao.findAll();
+        assertTrue(kebabDao.findAll() instanceof List);
+        Iterator i = kebabList.iterator();
+        while(i.hasNext()){
+            assertTrue(i.next() instanceof Kebab);
+        }
+
     }
 }
