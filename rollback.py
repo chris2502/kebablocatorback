d04ce754af6fc685a65aa04c5b1af703451caabe
vchaillou <valentin.chaillou@laposte.net>
2016-12-02 08:10:26 +0100

    Correction

--- rollback.py
+++ rollback.py
@@ -1,5 +1,5 @@
 ﻿# Valentin Chaillou
-# Script qui checke périodiquement le nombre d'erreurs 500 du serveur back
+# Script qui checke periodiquement le nombre d'erreurs 500 du serveur back
 
 import urllib.request
 import time
@@ -8,7 +8,7 @@ from subprocess import call
 
 URL_GET_ERROR = os.environ["URL_GET_ERROR"] # "http://localhost:8090/kebab/geterror"
 
-# Nombre d'erreurs autorisées
+# Nombre d'erreurs autorisees
 MAX_ERRORS_LEVEL_1 = int(os.environ["MAX_ERRORS_LEVEL_1"])
 MAX_ERRORS_LEVEL_2 = int(os.environ["MAX_ERRORS_LEVEL_2"])
 MAX_ERRORS_LEVEL_3 = int(os.environ["MAX_ERRORS_LEVEL_3"])
@@ -18,7 +18,7 @@ TIME_LEVEL_1 = int(os.environ["TIME_LEVEL_1"])
 TIME_LEVEL_2 = int(os.environ["TIME_LEVEL_2"])
 TIME_LEVEL_3 = int(os.environ["TIME_LEVEL_3"])
 
-# Fréquence des checks (nombre de checks par minute)
+# Frequence des checks (nombre de checks par minute)
 CHECKS_LEVEL_1 = int(os.environ["CHECKS_LEVEL_1"])
 CHECKS_LEVEL_2 = int(os.environ["CHECKS_LEVEL_2"])
 CHECKS_LEVEL_3 = int(os.environ["CHECKS_LEVEL_3"])
@@ -41,11 +41,11 @@ PROPERTIES_LEVELS = [
 	}
 ]
 
-# Juste pour être sûr que le serveur ait eu le temps de démarrer
-# Au-delà, il y a un problème
+# Juste pour être sûr que le serveur ait eu le temps de demarrer
+# Au-delà, il y a un probleme
 time.sleep(60) # secondes
 
-# On arrange les temps pour exclure les durées des niveaux précédents
+# On arrange les temps pour exclure les durees des niveaux precedents
 for i in range(1, len(PROPERTIES_LEVELS)):
 	PROPERTIES_LEVELS[i]["time"] -= PROPERTIES_LEVELS[i-1]["time"]
 
@@ -56,8 +56,8 @@ def rollback():
 
 # 3 niveaux
 # 1er niveau : pas plus de 20 erreurs en 10 minutes
-# 2ème niveau ; pas plus de 100 erreurs en 40 minutes
-# 3ème niveau : pas plus de 150 erreurs en 120 minutes
+# 2eme niveau ; pas plus de 100 erreurs en 40 minutes
+# 3eme niveau : pas plus de 150 erreurs en 120 minutes
 for levelProperty in PROPERTIES_LEVELS:
 	for i in range(0, levelProperty["time"] * levelProperty["checks"]):
 		toRollback = False
@@ -65,16 +65,16 @@ for levelProperty in PROPERTIES_LEVELS:
 			nbErreurs = int(urllib.request.urlopen(URL_GET_ERROR).read())
 			if nbErreurs > levelProperty["errors"]:
 				toRollback = True
-				print("Erreurs : " + str(nbErreurs) + " en " + str(i*60/levelProperty["checks"]) + " secondes. Préparation pour rollback...")
+				print("Erreurs : " + str(nbErreurs) + " en " + str(i*60/levelProperty["checks"]) + " secondes. Preparation pour rollback...")
 			else:
 				toRollback = False
 				print("Erreurs : " + str(nbErreurs) + " en " + str(i*60/levelProperty["checks"]) + " secondes")
 		except:
 			toRollback = True
-			print("Connexion perdue. Préparation pour rollback...")
+			print("Connexion perdue. Preparation pour rollback...")
 		if toRollback:
 			rollback()
 		
 		time.sleep(60/levelProperty["checks"])
-	print("\n************************\nNiveau passé avec succès\n************************\n")
+	print("\n************************\nNiveau passe avec succes\n************************\n")
 			
